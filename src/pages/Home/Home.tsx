28a853e7a5c323b47560f4a7c71d07630cc2f5f7
Stefan Vojvodic <stefan.vojvodic@gamecredits.com>
2019-09-06 22:58:37 +0200

    initial project setup

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import PageLayout from 'components/PageLayout';
+
+const Home: React.FC = () => {
+  return <PageLayout>Home page</PageLayout>;
+};
+
+export default Home;
